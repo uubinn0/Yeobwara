375db3ba933ab45ca3f3cbbb9c62a7292f5b6960
나유빈 <ub.devl0908@gmail.com>
2025-05-06 17:22:01 +0900

    :sparkles: feat: landing 페이지 라우팅

--- frontend/react/src/router/index.tsx
+++ frontend/react/src/router/index.tsx
@@ -1,18 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import LandingPage from "../pages/landing/page";
 import HelloWorld from "../pages/HelloWorld/page";
-// import HomePage from "../pages/home/page";
 import LoginPage from "../pages/login/page";
 import SignupPage from "../pages/signup/page";
-// import ChatPage from "../pages/chat/page";
+
 
 const Router = () => {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/helloworld" element={<HelloWorld />} />
-      {/* <Route path="/" element={<HomePage />} /> */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      {/* <Route path="/chat" element={<ChatPage />} /> */}
     </Routes>
   );
 };
